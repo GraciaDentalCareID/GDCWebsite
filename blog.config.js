70f79f55c8cc3c113a84d759e03e3622033effee
GraciaDentalCareID <114275373+GraciaDentalCareID@users.noreply.github.com>
2022-09-24 12:37:51 +0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -37,7 +37,7 @@ const BLOG = {
   telegramChatId: '263895784', // The chat id of your Telegram bot
   telegramChannelUrl: 'graciadentalcareid.notion.site', // The link of your Telegram channel
   telegramChannelName: 'zuolan_me', // The name of your Telegram channel
-  craftConfigShareUrl: 'https://www.craft.do/s/KQTJd0iW2LjZPU', // The link to share your craft config
+  craftConfigShareUrl: 'https://www.craft.do/s/3aXfRFmLUXU3w4', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee and Umami, please fill with 'ga' or 'ackee' or 'umami', leave it empty to disable it.
     umamiConfig: {
